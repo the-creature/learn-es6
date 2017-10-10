3a27bf55494379c0f6293fb8ce553513c8516105
Liccy Fuentes <liccyfuentes@gmail.com>
2017-10-09 23:38:24 -0500

    async with promise chain (ES6)

--- async-promise-chain/index.js
+++ async-promise-chain/index.js
@@ -3,12 +3,11 @@ const axios = require('axios');
 async function getStarWarsMaleCharacters(type) {
   const url = `https://swapi.co/api/${type}/`;
   const res = await axios.get(url);
-  const data = await JSON.stringify(res.data.results);
-  const results = JSON.parse(data);
-  const maleCharacters = results.map(person => {
+  const maleCharacters = res.data.results.map(person => {
     return person.gender === 'male' && person.name;
   });
 
-  console.log(maleCharacters.filter(Boolean));
+  return maleCharacters.filter(Boolean);
 }
-getStarWarsMaleCharacters('people');
+
+getStarWarsMaleCharacters('people').then(value => console.log(value));
