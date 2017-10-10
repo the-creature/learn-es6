7c86919b8046784bf12c04094fef335b080efea3
Liccy Fuentes <liccyfuentes@gmail.com>
2017-10-09 22:55:19 -0500

    async + await + Promise + es6 (converting Promises to synchronize function)

---
+++ async/async-await.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function getStarWarsMaleCharacters(type) {
+  const url = `https://swapi.co/api/${type}/`;
+  const res = await axios.get(url);
+  const data = await JSON.stringify(res.data.results);
+  const results = JSON.parse(data);
+  const maleCharacters = results.map(person => {
+    return person.gender === 'male' && person.name;
+  });
+
+  console.log(maleCharacters.filter(Boolean));
+}
+getStarWarsMaleCharacters('people');
